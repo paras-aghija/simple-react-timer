39a73682b74052b566e41558f2d9439b360a403c
paras-aghija <parasaghija2000@gmail.com>
2021-04-17 23:43:12 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const [timer, setTimer] = useState(0)
+  const [start, setStart] = useState(false)
+  const [stop, setStop] = useState(true)
+  const countRef = useRef(null)
+
+  const handleStart = () => {
+    if(start){return}
+    console.log("started");
+    setStart(true)
+    setStop(false)
+    countRef.current = setInterval(()=>{
+      setTimer((timer) => timer+1)
+    },1500)
+    // 1 added after every 1500 milisecs
+  }
+
+  const handleStop = () => {
+    if(stop){return}
+    console.log(timer);
+    clearInterval(countRef.current)
+    setStart(false)
+    setStop(true)
+  }
+
+  const handleReset = () => {
+    clearInterval(countRef.current)
+    setStart(false)
+    setStop(true)
+    setTimer(0)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Timer</h1>
+      <h2>{ timer }</h2>
+      <button onClick={handleStart}> START </button>
+      <button onClick={handleStop}> STOP </button>
+      <button onClick={handleReset}> RESET </button>
     </div>
   );
 }
